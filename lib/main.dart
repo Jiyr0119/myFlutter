ee6974e4dbabfed538393f6fbe2da867fd1850ff
Jonathan <594402100@qq.com>
2019-09-12 16:55:32 +0800

    route -  list,param

--- lib/main.dart
+++ lib/main.dart
@@ -156,10 +156,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'My Flutter',
-      theme: new ThemeData(primaryColor: Colors.blue),
-      home: new MyHomePage(title: 'Flutter Home Page'),
-    );
+        title: 'My Flutter',
+        initialRoute: "/", //名为"/"的路由作为应用的home(首页)
+        theme: new ThemeData(primaryColor: Colors.blue),
+        // home: new MyHomePage(title: 'Flutter Home Page'),
+        //注册路由表
+        routes: {
+          "tip2": (context) {
+            return TipRoute(text: ModalRoute.of(context).settings.arguments);
+          },
+          "new_page": (context) => NewRoute(),
+          "/": (context) =>
+              MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+        });
   }
 }
 
@@ -200,9 +209,21 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Text('open new route'),
               textColor: Colors.blue,
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return NewRoute();
-                }));
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context).pushNamed("new_page", arguments: "hi");
+              },
+            ),
+            FlatButton(
+              child: Text('RouterTestRoute'),
+              textColor: Colors.blue,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return RouterTestRoute();
+                // }));
+                Navigator.of(context).pushNamed("tip2", arguments: "hi");
               },
             )
           ],
@@ -220,12 +241,71 @@ class _MyHomePageState extends State<MyHomePage> {
 class NewRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         title: Text("New route"),
       ),
       body: Center(
         child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text, // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("提示"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是高俊杰"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          // 打开`TipRoute`，并等待��回结果
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return TipRoute(
+                  // 路由参数
+                  text: "flutter",
+                );
+              },
+            ),
+          );
+          //输出`TipRoute`路由返回结果
+          print("路由返回值: $result");
+        },
+        child: Text("打开提示页"),
       ),
     );
   }
