b17b7b484784e1625471de3a289a72d0e55a0309
Jonathan <594402100@qq.com>
2019-09-15 18:00:12 +0800

    state tapBoxa

--- lib/main.dart
+++ lib/main.dart
@@ -149,6 +149,10 @@
 // }
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/widgets.dart';
+//导入cupertino widget库
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(new MyApp());
 
@@ -166,9 +170,34 @@ class MyApp extends StatelessWidget {
             return TipRoute(text: ModalRoute.of(context).settings.arguments);
           },
           "new_page": (context) => NewRoute(),
+          "new_widget": (context) => NewWidget(),
+          "new_context": (context) => ContextRoute(),
+          "new_counter": (context) => CounterRoute(),
+          "new_snack": (context) => SnackRoute(),
+          "new_cuper": (context) => CupertinoTestRoute(),
           "/": (context) =>
               MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+        },
+        onGenerateRoute: (RouteSettings settings) {
+          return MaterialPageRoute(builder: (context) {
+            String routeName = settings.name;
+            print("路由名字 ,$routeName");
+            // 如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，
+            // 引导用户登录；其它情况则正常打开路由。
+          });
         });
+  }
+}
+
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
+    );
   }
 }
 
@@ -205,6 +234,8 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            RandomWordsWidget(),
+            TapboxA(),
             FlatButton(
               child: Text('open new route'),
               textColor: Colors.blue,
@@ -213,7 +244,68 @@ class _MyHomePageState extends State<MyHomePage> {
                 //   return NewRoute();
                 // }));
                 // Navigator.pushNamed(context, "new_page");
-                Navigator.of(context).pushNamed("new_page", arguments: "hi flutter");
+                Navigator.of(context)
+                    .pushNamed("new_page", arguments: "hi flutter");
+              },
+            ),
+            FlatButton(
+              child: Text('open new widget'),
+              textColor: Colors.blue,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context)
+                    .pushNamed("new_widget", arguments: "hi flutter");
+              },
+            ),
+            FlatButton(
+              child: Text('open new context'),
+              textColor: Colors.blue,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context)
+                    .pushNamed("new_context", arguments: "hi flutter");
+              },
+            ),
+            FlatButton(
+              child: Text('open new counter'),
+              textColor: Colors.pink,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context)
+                    .pushNamed("new_counter", arguments: "hi flutter");
+              },
+            ),
+            FlatButton(
+              child: Text('open new snack'),
+              textColor: Colors.pink,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context)
+                    .pushNamed("new_snack", arguments: "hi flutter");
+              },
+            ),
+            FlatButton(
+              child: Text('open new cuper'),
+              textColor: Colors.pink,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context)
+                    .pushNamed("new_cuper", arguments: "hi flutter");
               },
             ),
             FlatButton(
@@ -223,7 +315,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 // Navigator.push(context, MaterialPageRoute(builder: (context) {
                 //   return RouterTestRoute();
                 // }));
-                Navigator.of(context).pushNamed("tip2", arguments: "hi flutter");
+                Navigator.of(context)
+                    .pushNamed("tip2", arguments: "hi flutter");
               },
             )
           ],
@@ -241,7 +334,7 @@ class _MyHomePageState extends State<MyHomePage> {
 class NewRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var args = ModalRoute.of(context).settings.arguments;
+    // var args = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         title: Text("New route"),
@@ -250,6 +343,23 @@ class NewRoute extends StatelessWidget {
         child: Text("This is new route"),
       ),
     );
+    // return Echo(text: "hello world");
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // var args = ModalRoute.of(context).settings.arguments;
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("New route"),
+    //   ),
+    //   body: Center(
+    //     child: Text("This is new route"),
+    //   ),
+    // );
+    return Echo(text: "hello world");
   }
 }
 
@@ -306,6 +416,219 @@ class RouterTestRoute extends StatelessWidget {
           print("路由返回值: $result");
         },
         child: Text("打开提示页"),
+      ),
+    );
+  }
+}
+
+class Echo extends StatelessWidget {
+  const Echo({
+    Key key,
+    @required this.text,
+    this.backgroundColor: Colors.lightBlue,
+  }) : super(key: key);
+
+  final String text;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New widget"),
+      ),
+      body: Center(
+        child: Text(text),
+      ),
+    );
+    // return Center(
+    //   child: Container(
+    //     // color: ,
+    //     child: Text(text),
+    //   ),
+    // );
+  }
+}
+
+class ContextRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Context测试"),
+      ),
+      body: Container(
+        child: Builder(builder: (context) {
+          // 在Widget树中向上查找最近的父级`Scaffold` widget
+          Scaffold scaffold = context.ancestorWidgetOfExactType(Scaffold);
+          // 直接返回 AppBar的title， 此处实际上是Text("Context测试")
+          return (scaffold.appBar as AppBar).title;
+        }),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({Key key, this.initValue: 0});
+
+  final int initValue;
+
+  @override
+  _CounterWidgetState createState() => new _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    //初始化状态
+    _counter = widget.initValue;
+    print("initState");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter"),
+      ),
+      body: Center(
+        child: FlatButton(
+          child: Text('$_counter'),
+          //点击后计数器自增
+          onPressed: () => setState(
+            () => ++_counter,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void didUpdateWidget(CounterWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print("deactive");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
+}
+
+class CounterRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CounterWidget();
+  }
+}
+
+class SnackRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("子树中获取State对象"),
+      ),
+      body: Center(
+        child: Builder(builder: (context) {
+          return RaisedButton(
+            onPressed: () {
+              // 查找父级最近的Scaffold对应的ScaffoldState对象
+              ScaffoldState _state =
+                  context.ancestorStateOfType(TypeMatcher<ScaffoldState>());
+              //调用ScaffoldState的showSnackBar来弹出SnackBar
+              _state.showSnackBar(
+                SnackBar(
+                  content: Text("我是SnackBar"),
+                ),
+              );
+            },
+            child: Text("显示SnackBar"),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class CupertinoTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Cupertino Demo"),
+      ),
+      child: Center(
+        child: CupertinoButton(
+            color: CupertinoColors.activeBlue,
+            child: Text("Press"),
+            onPressed: () {
+              print("hello,world");
+            }),
+      ),
+    );
+  }
+}
+
+// TapboxA 管理自身状态.
+
+//------------------------- TapboxA ----------------------------------
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => new _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            _active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
       ),
     );
   }
