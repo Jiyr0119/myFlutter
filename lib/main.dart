2e7a3fcd3a8f7d93627c2dddd38aaad740f41d62
Jonathan <594402100@qq.com>
2019-09-15 18:13:26 +0800

    state 3.2

--- lib/main.dart
+++ lib/main.dart
@@ -175,6 +175,7 @@ class MyApp extends StatelessWidget {
           "new_counter": (context) => CounterRoute(),
           "new_snack": (context) => SnackRoute(),
           "new_cuper": (context) => CupertinoTestRoute(),
+          "new_state": (context) => StateTest(),
           "/": (context) =>
               MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
         },
@@ -235,7 +236,6 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.display1,
             ),
             RandomWordsWidget(),
-            TapboxA(),
             FlatButton(
               child: Text('open new route'),
               textColor: Colors.blue,
@@ -309,6 +309,18 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             FlatButton(
+              child: Text('open new state'),
+              textColor: Colors.pink,
+              onPressed: () {
+                // Navigator.push(context, MaterialPageRoute(builder: (context) {
+                //   return NewRoute();
+                // }));
+                // Navigator.pushNamed(context, "new_page");
+                Navigator.of(context)
+                    .pushNamed("new_state", arguments: "hi flutter");
+              },
+            ),
+            FlatButton(
               child: Text('RouterTestRoute'),
               textColor: Colors.blue,
               onPressed: () {
@@ -318,7 +330,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.of(context)
                     .pushNamed("tip2", arguments: "hi flutter");
               },
-            )
+            ),
           ],
         ),
       ),
@@ -594,6 +606,27 @@ class CupertinoTestRoute extends StatelessWidget {
   }
 }
 
+// TapboxA(),
+// ParentWidget(),
+class StateTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // var args = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("New route"),
+        ),
+        body: new Center(
+            child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[TapboxA(), ParentWidget(), ParentWidgetC()]))
+        // child: Text("This is new route"),
+
+        );
+    // return Echo(text: "hello world");
+  }
+}
+
 // TapboxA 管理自身状态.
 
 //------------------------- TapboxA ----------------------------------
@@ -628,6 +661,162 @@ class _TapboxAState extends State<TapboxA> {
         height: 200.0,
         decoration: new BoxDecoration(
           color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+// ParentWidget 为 TapboxB 管理状态.
+
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+//---------------------------- ParentWidget ----------------------------
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//----------------------------- TapboxC ------------------------------
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapboxCState createState() => new _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 在按下时添加绿色边框，当抬起时，取消高亮
+    return new GestureDetector(
+      onTapDown: _handleTapDown, // 处理按下事件
+      onTapUp: _handleTapUp, // 处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(widget.active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? new Border.all(
+                  color: Colors.teal[700],
+                  width: 10.0,
+                )
+              : null,
         ),
       ),
     );
