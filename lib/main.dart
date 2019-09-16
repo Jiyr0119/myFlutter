e967fe9ea03ed86c5c6b9faef0971145e5df7f30
Jonathan <594402100@qq.com>
2019-09-16 20:50:40 +0800

    progress  3.8

--- lib/main.dart
+++ lib/main.dart
@@ -359,11 +359,82 @@ class NewRoute extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            //  FormTestRoute(),
-            FocusTestRoute()
+            TextField(
+              autofocus: true,
+              decoration: InputDecoration(
+                  labelText: "用户名",
+                  hintText: "用户名或邮箱",
+                  prefixIcon: Icon(Icons.person)),
+              onChanged: (v) {
+                print("onChange:$v");
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "密码",
+                  hintText: "您的登录密码",
+                  prefixIcon: Icon(Icons.lock)),
+              obscureText: true,
+            ),
+            FocusTestRoute(),
+            // LinearProgressIndicator(
+            //   backgroundColor: Colors.grey[200],
+            //   valueColor: AlwaysStoppedAnimation(Colors.blue),
+            // ),
+            // CircularProgressIndicator(
+            //   backgroundColor: Colors.grey[200],
+            //   valueColor: AlwaysStoppedAnimation(Colors.blue),
+            // )
+            ProgressRoute()
           ],
         ));
     // return Echo(text: "hello world");
+  }
+}
+
+//ProgressRoute
+class ProgressRoute extends StatefulWidget {
+  @override
+  _ProgressRouteState createState() => _ProgressRouteState();
+}
+
+class _ProgressRouteState extends State<ProgressRoute>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    //动画执行时间3秒
+    _animationController =
+        new AnimationController(vsync: this, duration: Duration(seconds: 3));
+    _animationController.forward();
+    _animationController.addListener(() => setState(() => {}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: ColorTween(begin: Colors.grey, end: Colors.blue)
+                  .animate(_animationController), // 从灰色变成蓝色
+              value: _animationController.value,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -422,87 +493,6 @@ class _FocusTestRouteState extends State<FocusTestRoute> {
             },
           ),
         ],
-      ),
-    );
-  }
-}
-
-// form
-class FormTestRoute extends StatefulWidget {
-  @override
-  _FormTestRouteState createState() => new _FormTestRouteState();
-}
-
-class _FormTestRouteState extends State<FormTestRoute> {
-  TextEditingController _unameController = new TextEditingController();
-  TextEditingController _pwdController = new TextEditingController();
-  GlobalKey _formKey = new GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Form Test"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
-        child: Form(
-          key: _formKey, //设置globalKey，用于后面获取FormState
-          autovalidate: true, //开启自动校验
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                  autofocus: true,
-                  controller: _unameController,
-                  decoration: InputDecoration(
-                      labelText: "用户名",
-                      hintText: "用户名或邮箱",
-                      icon: Icon(Icons.person)),
-                  // 校验用户名
-                  validator: (v) {
-                    return v.trim().length > 0 ? null : "用户名不能为空";
-                  }),
-              TextFormField(
-                  controller: _pwdController,
-                  decoration: InputDecoration(
-                      labelText: "密码",
-                      hintText: "您的登录密码",
-                      icon: Icon(Icons.lock)),
-                  obscureText: true,
-                  //校验密码
-                  validator: (v) {
-                    return v.trim().length > 5 ? null : "密码不能少于6位";
-                  }),
-              // 登录按钮
-              Padding(
-                padding: const EdgeInsets.only(top: 28.0),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RaisedButton(
-                        padding: EdgeInsets.all(15.0),
-                        child: Text("登录"),
-                        color: Theme.of(context).primaryColor,
-                        textColor: Colors.white,
-                        onPressed: () {
-                          //在这里不能通过此方式获取FormState，context不对
-                          //print(Form.of(context));
-
-                          // 通过_formKey.currentState 获取FormState后，
-                          // 调用validate()方法校验用户名密码是否合法，校验
-                          // 通过后再提交数据。
-                          if ((_formKey.currentState as FormState).validate()) {
-                            //验证通过提交数据
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
